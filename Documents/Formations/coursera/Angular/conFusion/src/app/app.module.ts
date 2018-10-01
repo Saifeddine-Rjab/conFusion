d1c3cf9a1882e408e79eb7c69fc12f0b0b9e5734
Saifeddine-Rjab <saifeddineerjab@gmail.com>
2018-10-01 22:12:05 +0100

    Template Forms Part 1

--- Documents/Formations/coursera/Angular/conFusion/src/app/app.module.ts
+++ Documents/Formations/coursera/Angular/conFusion/src/app/app.module.ts
@@ -6,6 +6,12 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
+
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import 'hammerjs';
@@ -51,7 +57,14 @@ import { LoginComponent } from './login/login.component';
     MatCardModule,
     MatButtonModule,
     AppRoutingModule,
-    MatDialogModule
+    MatDialogModule,
+
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule
+
+   
   ],
   providers: [
     DishService,
